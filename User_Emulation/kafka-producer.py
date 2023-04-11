864443f5e13011c17c99af3971478654b49e00c2
Katy Humphries <katy.humphries96@gmail.com>
2023-04-11 19:41:59 +0100

    linked ML and retail producers to topics and added some test data

--- User_Emulation/kafka-producer.py
+++ User_Emulation/kafka-producer.py
@@ -1,12 +1,51 @@
 from kafka import KafkaProducer
 from json import dumps
 
-producer = KafkaProducer(
-    bootstrap_servers="localhost:9092",
-    value_serializer = lambda x: dumps(x).encode("utf-8")
-)
+ml_models = [ #testdata
+    {
+        "Model_name": "ResNet-50",
+        "Accuracy": "92.1",
+        "Framework_used": "Pytorch"
+    },
+    {
+        "Model_name": "Random Forest",
+        "Accuracy": "82.7",
+        "Framework_used": "SKLearn"
+    }
+] 
 
-json_data = {"User ID": "AWGF76KYU", "Event": "webpage.open"}
+retail_data = [ #testdata
+    {
+        "Item": "42 LCD TV",
+        "Price": "209.99",
+        "Quantity": "1"
+    },
+    {
+        "Item": "Large Sofa",
+        "Price": "259.99",
+        "Quantity": 2
+    }
+]
 
-producer.send("kafkaPythonTest", json_data)
-producer.flush()
+class MLProducer:
+    ml_producer = KafkaProducer(
+        bootstrap_servers="localhost:9092",
+        client_id = "ML data producer",
+        value_serializer = lambda mlmsg: dumps(mlmsg).encode("ascii")
+    )
+    for mlmsg in ml_models:
+        ml_producer.send(topic = "MLData, value = mlmsg")
+    
+    ml_producer.flush()
+
+class RetailProducer:
+    retail_producer = KafkaProducer(
+        bootstrap_servers = "localhost:9092",
+        client_id = "ML data producer",
+        value_serializer = lambda retailmsg: dumps(retailmsg).encode("ascii")
+    )
+    for retailmsg in retail_data:
+        retail_producer.send(topic = "Retaildata", value = retailmsg)
+    
+    retail_producer.flush()
+
