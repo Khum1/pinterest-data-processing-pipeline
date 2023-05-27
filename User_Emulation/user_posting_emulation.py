6305de4e0de1daf2a9e21611e9dd95d800982674
Katy Humphries <katy.humphries96@gmail.com>
2023-05-27 18:09:13 +0100

    overhaul of previous project. created requirements.txt

--- User_Emulation/user_posting_emulation.py
+++ User_Emulation/user_posting_emulation.py
@@ -4,9 +4,8 @@ import random
 from multiprocessing import Process
 import boto3
 import json
-from sqlalchemy import text, create_engine
-from kafka import KafkaProducer
-
+import sqlalchemy
+from sqlalchemy import text
 
 
 random.seed(100)
@@ -23,39 +22,45 @@ class AWSDBConnector:
         self.PORT = 3306
         
     def create_db_connector(self):
-        engine = create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
+        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
         return engine
 
 
 new_connector = AWSDBConnector()
-kafka_producer = KafkaProducer(
-        bootstrap_servers="localhost:9092",
-        client_id = "ML data producer",
-        value_serializer = lambda mlmsg: json.dumps(mlmsg, default = str).encode("ascii")
-        )
+
 
 def run_infinite_post_data_loop():
     while True:
         sleep(random.randrange(0, 2))
         random_row = random.randint(0, 11000)
         engine = new_connector.create_db_connector()
+
         with engine.connect() as connection:
-            query = f"SELECT * FROM pinterest_data LIMIT {random_row}, 1"
-            selected_row = connection.execute(text(query))
-            result = dict(selected_row.mappings().all()[0])
-        # for row in selected_row:
-        #     print(selected_row)
-        #     result = dict(row)
-            print(result)
-            requests.post("http://localhost:8000/pin/", json=result)
-            msg = json.dumps(result, default = str).encode("ascii")
-            kafka_producer.send("MLData", value = msg)
+
+            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
+            pin_selected_row = connection.execute(pin_string)
+            
+            for row in pin_selected_row:
+                pin_result = dict(row._mapping)
+
+            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+            geo_selected_row = connection.execute(geo_string)
+            
+            for row in geo_selected_row:
+                geo_result = dict(row._mapping)
+
+            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+            user_selected_row = connection.execute(user_string)
+            
+            for row in user_selected_row:
+                user_result = dict(row._mapping)
+            
+            print(pin_result)
+            print(geo_result)
+            print(user_result)
 
 
 if __name__ == "__main__":
     run_infinite_post_data_loop()
     print('Working')
     
-    
-
-
